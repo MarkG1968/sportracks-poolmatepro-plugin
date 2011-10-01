80a7da288754881b93019616e7ce4668fc94fea5
mark.gravestock@gmail.com <mark.gravestock@gmail.com@8f0fc38f-8859-4efc-5e4b-8ebdc44335ca>
2011-10-01 08:22:02 +0000

    Create initial release.

---
+++ tags/0.1/PoolMateProSportTrackPlugIn/Domain/SwimSessionFactory.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mark Gravestock
+ * Date: 18/09/2011
+ * Time: 01:10
+ * 
+ * User: © Mark Gravestock 
+*/
+using System;
+using System.Collections.Generic;
+
+using MarkGravestock.SportTracks.PlugIns.PoolMatePro.File;
+	
+namespace MarkGravestock.SportTracks.PlugIns.PoolMatePro.Domain
+{
+	/// <summary>
+	/// Description of ActivityFactory.
+	/// </summary>
+	public class SwimSessionFactory
+	{
+		public SwimSessionFactory()
+		{
+		}
+		
+		public IList<SwimSession> CreateFrom(IList<LogEntry> entries)
+		{
+			IDictionary<DateTime, SwimSession> activityByStart = new Dictionary<DateTime, SwimSession>();
+
+			foreach(LogEntry entry in entries)
+			{
+				if (Activity.Equals(entry.Activity, Activity.Swim))
+				{
+					DateTime entryDateTime = new DateTime(entry.LogDate.Year, entry.LogDate.Month, entry.LogDate.Day, entry.LogTime.Hour, entry.LogTime.Minute, entry.LogTime.Second, DateTimeKind.Local);
+	
+					SwimSet set = new SwimSet(entry);
+					
+					if (!activityByStart.ContainsKey(entryDateTime))
+	                {
+						activityByStart.Add(entryDateTime, new SwimSession(entryDateTime, set));
+	                }
+					else
+					{
+						activityByStart[entryDateTime].AddSet(set);
+					}
+				}
+			}
+			
+			List<SwimSession> activities = new List<SwimSession>();
+			activities.AddRange(activityByStart.Values);
+			
+			return activities;
+		}
+	}
+}
---
+++ tags/0.1/PoolMateProSportTrackPlugIn/ValidationStatus.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mark Gravestock
+ * Date: 28/09/2011
+ * Time: 19:41
+ * 
+ * User: © Mark Gravestock 
+*/
+using System;
+
+namespace MarkGravestock.SportTracks.PlugIns.PoolMatePro
+{
+	/// <summary>
+	/// Description of ValidationStatus.
+	/// </summary>
+	public enum ValidationStatus
+	{
+		NotValidated,
+		Valid,
+		Invalid
+	}
+}
