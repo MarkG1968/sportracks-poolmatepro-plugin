7b3a5943067b53217ca0c33066daded441fb5cfd
mark.gravestock@gmail.com <mark.gravestock@gmail.com@8f0fc38f-8859-4efc-5e4b-8ebdc44335ca>
2011-10-01 09:08:56 +0000

    Create initial release to build download from.

---
+++ tags/0.1/PoolMateProSportTrackPlugIn/Domain/SwimSession.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mark Gravestock
+ * Date: 18/09/2011
+ * Time: 01:09
+ * 
+ * User: © Mark Gravestock 
+*/
+using System;
+using System.Collections.Generic;
+
+namespace MarkGravestock.SportTracks.PlugIns.PoolMatePro.Domain
+{
+	/// <summary>
+	/// Description of Activity.
+	/// </summary>
+	public class SwimSession
+	{
+		List<SwimSet> sets = new List<SwimSet>();
+		SwimSet firstSet = null;
+		DateTime startTime = DateTime.Today;
+		
+		public SwimSession(DateTime startTime, SwimSet set)
+		{
+			this.startTime = startTime;
+			this.firstSet = set;
+			
+			AddSet(set);
+		}
+		
+		public DateTime StartTime
+		{
+			get { return startTime; }
+		}
+		
+		public void AddSet(SwimSet set)
+		{
+			sets.Add(set);
+		}
+		
+		public int SetCount
+		{
+			get { return sets.Count; }
+		}
+
+		public List<SwimSet> Sets
+		{
+			get { return sets; }
+		}
+		
+		public TimeSpan TotalTime
+		{
+			get { return firstSet.TotalTime; }
+		}
+		
+		public String PoolLengthDescription
+		{
+			get { return firstSet.PoolLengthDescription; }
+		}
+
+		public float TotalDistanceMeters
+		{
+			get { return firstSet.TotalDistanceMeters; }
+		}
+		
+		public float TotalCalories
+		{
+			get { return firstSet.Calories; }
+		}
+
+	}
+}
---
+++ tags/0.1/PoolMateProSportTrackPlugIn/ValidateEventHandler.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mark Gravestock
+ * Date: 30/09/2011
+ * Time: 21:37
+ * 
+ * User: © Mark Gravestock 
+*/
+using System;
+
+namespace MarkGravestock.SportTracks.PlugIns.PoolMatePro
+{
+	/// <summary>
+	/// Description of ValidateEventHandler.
+	/// </summary>
+	public delegate void ValidateEventHandler(DirectoryValidationEvent directoryValidationEvent);
+}
