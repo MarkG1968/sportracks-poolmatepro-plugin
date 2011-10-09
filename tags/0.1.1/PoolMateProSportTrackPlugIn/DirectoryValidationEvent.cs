971121ce8a8baf3e278f6962b3d1ddad1c1435b7
mark.gravestock@gmail.com <mark.gravestock@gmail.com@8f0fc38f-8859-4efc-5e4b-8ebdc44335ca>
2011-10-09 19:22:36 +0000

    Include pacakaging build for sporttracks plugin.

---
+++ tags/0.1.1/PoolMateProSportTrackPlugIn/DirectoryValidationEvent.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mark Gravestock
+ * Date: 28/09/2011
+ * Time: 19:38
+ * 
+ * User: © Mark Gravestock 
+*/
+using System;
+using System.IO;
+
+namespace MarkGravestock.SportTracks.PlugIns.PoolMatePro
+{
+	/// <summary>
+	/// Description of DirectoryValidationEvent.
+	/// </summary>
+	public class DirectoryValidationEvent
+	{
+		private DirectoryInfo directoryToValidate;
+		private ValidationStatus validationStatus;
+		
+		public DirectoryValidationEvent(DirectoryInfo directory)
+		{
+			this.directoryToValidate = directory;
+			this.ValidationStatus = ValidationStatus.NotValidated;
+		}
+		
+		public ValidationStatus ValidationStatus
+		{
+			get { return validationStatus; }
+			set { validationStatus = value; }
+		}
+		
+		public DirectoryInfo DirectoryToValidate
+		{
+			get { return directoryToValidate; }
+		}
+	}
+}
